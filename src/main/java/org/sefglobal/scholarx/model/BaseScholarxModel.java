8b0dea30ca601d68c70bd5029de8ddedfd062867
piumal1999 <piumalrathnayake@hotmail.com>
2020-08-18 12:35:39 +0530

    Add annotations to generate tables

--- src/main/java/org/sefglobal/scholarx/model/BaseScholarxModel.java
+++ src/main/java/org/sefglobal/scholarx/model/BaseScholarxModel.java
@@ -1,11 +1,34 @@
 package org.sefglobal.scholarx.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"})
 public abstract class BaseScholarxModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "createdAt",
+            nullable = false,
+            updatable = false)
+    @CreatedDate
     private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updatedAt",
+            nullable = false)
+    @LastModifiedDate
     private Date updatedAt;
 
     public int getId() {
--- src/main/java/org/sefglobal/scholarx/model/EnrolledUser.java
+++ src/main/java/org/sefglobal/scholarx/model/EnrolledUser.java
@@ -2,9 +2,21 @@ package org.sefglobal.scholarx.model;
 
 import org.sefglobal.scholarx.util.EnrolmentState;
 
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class EnrolledUser extends BaseScholarxModel{
+
+    @ManyToOne(optional = false)
     private Profile profile;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10,
+            nullable = false)
     private EnrolmentState state;
+
+    @ManyToOne(optional = false)
     private Program program;
 
     public Profile getProfile() {
