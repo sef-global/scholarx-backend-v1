0d402f408ec6f681777c8c1727f186239ecd4c48
Piumal Rathnayake <piumalrathnayake@hotmail.com>
2020-10-15 18:47:46 +0530

    Fix infinite loops due to circular dependancies (#49)
    Issue due to circular dependencies with one to many relationships in the JPA Entities. As a fix children of all parent classes have been ignored at the JSON level.

--- src/main/java/org/sefglobal/scholarx/model/BaseScholarxModel.java
+++ src/main/java/org/sefglobal/scholarx/model/BaseScholarxModel.java
@@ -1,6 +1,5 @@
 package org.sefglobal.scholarx.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,7 +17,6 @@ import java.util.Date;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"createdAt", "updatedAt"})
 public abstract class BaseScholarxModel implements Serializable {
 
     @Id
