e675ffeb09539913508db1b2ec2b9c06e8d6db73
Gimhan-minion <kavindrawijayawardana2000@gmail.com>
2021-06-19 11:13:04 +0530

    Implement email template feature

---
+++ src/main/java/org/sefglobal/scholarx/config/ThymeleafTemplateConfig.java
@@ -0,0 +1,30 @@
+package org.sefglobal.scholarx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class ThymeleafTemplateConfig {
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver(){
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix("classpath:/templates/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+}
--- src/main/java/org/sefglobal/scholarx/service/EmailService.java
+++ src/main/java/org/sefglobal/scholarx/service/EmailService.java
@@ -1,21 +1,34 @@
 package org.sefglobal.scholarx.service;
 import org.sefglobal.scholarx.model.Email;
 import org.sefglobal.scholarx.util.EmailUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private EmailUtil emailUtil;
+    private final EmailUtil emailUtil;
 
-    public Email sendEmail(String emailAddress, String subject, String message){
+    public EmailService(EmailUtil emailUtil) {
+        this.emailUtil = emailUtil;
+    }
+
+    public Email sendEmail(String emailAddress, String subject, String message) throws IOException, MessagingException {
         Email email = new Email();
         email.setEmail(emailAddress);
         email.setSubject(subject);
         email.setMessage(message);
+
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("emailAddress", emailAddress);
+        model.put("subject", subject);
+        model.put("message", message);
+        email.setProps(model);
+
         emailUtil.sendSimpleMessage(email);
 
         return email;
