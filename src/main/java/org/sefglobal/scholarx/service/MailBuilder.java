489e1a1aafb077ab15c0c600433fe00e034f34d5
pasindur2 <pasindur2pj@gmail.com>
2022-07-04 13:45:14 +0530

    Make the mailbuilder connection singleton

---
+++ src/main/java/org/sefglobal/scholarx/service/MailBuilder.java
@@ -0,0 +1,31 @@
+package org.sefglobal.scholarx.service;
+
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.mailer.MailerBuilder;
+
+public class MailBuilder {
+
+    private static MailBuilder mailBuilder_instance = null;
+
+    private String username = "pasindur2@gmail.com";
+    private String password = "wwooravkrkhorobx";
+    private String host = "smtp.mailtrap.io";
+    private int port = 2525;
+
+    private MailBuilder(Email email){
+
+        MailerBuilder
+                .withSMTPServer(host, port, username, password)
+                .buildMailer()
+                .sendMail(email);
+    }
+
+    public static MailBuilder getInstance(Email email)
+    {
+        if (mailBuilder_instance == null)
+            mailBuilder_instance = new MailBuilder(email);
+
+        return mailBuilder_instance;
+    }
+
+}
--- src/main/java/org/sefglobal/scholarx/service/SimpleJavaMail.java
+++ src/main/java/org/sefglobal/scholarx/service/SimpleJavaMail.java
@@ -2,28 +2,21 @@ package org.sefglobal.scholarx.service;
 
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.email.EmailBuilder;
-import org.simplejavamail.mailer.MailerBuilder;
 
 public class SimpleJavaMail {
 
-    public void sendEmail(String fname,String emailAddress, String subject, String message, boolean showButton) {
+    public void sendEmail(String name,String emailAddress, String subject, String message, boolean showButton) {
 
         String username = "pasindur2@gmail.com";
-        String password = "wwooravkrkhorobx";
-        String host = "smtp.mailtrap.io";
-        int port = 2525;
 
         Email email = EmailBuilder.startingBlank()
                 .from("Sustainable Education Foundation", username)
-                .to(fname, emailAddress)
+                .to(name, emailAddress)
                 .withSubject(subject)
                 .withHTMLText(message)
                 .buildEmail();
 
-        MailerBuilder
-                .withSMTPServer(host, port, username, password)
-                .buildMailer()
-                .sendMail(email);
+        MailBuilder.getInstance(email);
 
         System.out.println("Email sent successfully");
     }
