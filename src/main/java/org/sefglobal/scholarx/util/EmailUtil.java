25a9460a9f5e6f4594360ddc13808aa1cba858f8
pasindur2 <pasindur2pj@gmail.com>
2022-07-31 18:57:35 +0530

    Add dependancy and set senders email manually

--- src/main/java/org/sefglobal/scholarx/util/EmailUtil.java
+++ src/main/java/org/sefglobal/scholarx/util/EmailUtil.java
@@ -4,6 +4,7 @@ import org.sefglobal.scholarx.model.Mail;
 import org.sefglobal.scholarx.service.MailConnection;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.email.EmailBuilder;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.context.Context;
@@ -12,6 +13,7 @@ import org.thymeleaf.context.Context;
 public class EmailUtil {
 
     private final SpringTemplateEngine templateEngine;
+    private static Environment env;
 
     public EmailUtil(SpringTemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
@@ -19,12 +21,14 @@ public class EmailUtil {
 
     public void sendEmail(Mail mail) {
 
+        String username = env.getProperty("spring.mail.username");
         Context context = new Context();
         context.setVariables(mail.getProps());
 
         String htmlText = templateEngine.process("scholarx", context);
 
          Email email = EmailBuilder.startingBlank()
+                .from(username)
                 .to(mail.getEmail())
                 .withSubject(mail.getSubject())
                 .withHTMLText(htmlText)
