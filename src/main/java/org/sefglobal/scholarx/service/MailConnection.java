7e6e220bc231560d65cc4877cf37228144719d38
pasindur2 <pasindur2pj@gmail.com>
2022-08-06 17:50:38 +0530

    Fix environment variable issue

--- src/main/java/org/sefglobal/scholarx/service/MailConnection.java
+++ src/main/java/org/sefglobal/scholarx/service/MailConnection.java
@@ -2,25 +2,15 @@ package org.sefglobal.scholarx.service;
 
 import org.simplejavamail.api.mailer.Mailer;
 import org.simplejavamail.mailer.MailerBuilder;
-import org.springframework.core.env.Environment;
 
 public class MailConnection {
-    private static Environment env;
 
-    private MailConnection(Environment env){
-        this.env = env;
-    }
-    private static String username = env.getProperty("spring.mail.username");
-    private static String password = env.getProperty("spring.mail.password");
-    private static String host = env.getProperty("spring.mail.host");
-    private static String port = env.getProperty("spring.mail.port");
+    private MailConnection(){}
     private static Mailer instance = null;
 
     public static Mailer getInstance() {
         if (instance == null){
-            instance = MailerBuilder
-                                    .withSMTPServer(host, Integer.valueOf(port), username, password)
-                                    .buildMailer();
+            instance = MailerBuilder.buildMailer();
         }
         return instance;
     }
--- src/main/java/org/sefglobal/scholarx/util/EmailUtil.java
+++ src/main/java/org/sefglobal/scholarx/util/EmailUtil.java
@@ -4,7 +4,6 @@ import org.sefglobal.scholarx.model.Mail;
 import org.sefglobal.scholarx.service.MailConnection;
 import org.simplejavamail.api.email.Email;
 import org.simplejavamail.email.EmailBuilder;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.context.Context;
@@ -13,7 +12,6 @@ import org.thymeleaf.context.Context;
 public class EmailUtil {
 
     private final SpringTemplateEngine templateEngine;
-    private static Environment env;
 
     public EmailUtil(SpringTemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
@@ -21,14 +19,12 @@ public class EmailUtil {
 
     public void sendEmail(Mail mail) {
 
-        String username = env.getProperty("spring.mail.username");
         Context context = new Context();
         context.setVariables(mail.getProps());
 
         String htmlText = templateEngine.process("scholarx", context);
 
          Email email = EmailBuilder.startingBlank()
-                .from(username)
                 .to(mail.getEmail())
                 .withSubject(mail.getSubject())
                 .withHTMLText(htmlText)
